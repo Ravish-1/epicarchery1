867f514db1fc05610748465a26c3173cd2f419ba
Ravish-1 <86870933+Ravish-1@users.noreply.github.com>
2021-08-17 17:54:15 +0530

    Add files via upload

---
+++ ComputerBase.js
@@ -0,0 +1,26 @@
+class ComputerBase 
+{
+  constructor(x, y, w, h) 
+  {
+    let options = {
+      isStatic:true
+    };
+    
+    this.body = Bodies.rectangle(x, y, w, h, options);
+    this.w = w;
+    this.h = h;
+    this.image=loadImage('assets/base2.png')
+    World.add(world, this.body);
+  }
+
+  show() {
+    var pos = this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x,pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image,0,0,this.w,this.h)
+    pop();
+  }
+}
